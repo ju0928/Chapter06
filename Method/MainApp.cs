17cf70e461a4627b982ebb0eb1f281c4505c2596
주연 장 <gg2222222@naver.com>
2023-03-20 15:29:43 +0900

    202303201529

---
+++ Method/MainApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Method
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            return a + b;
+        }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
+    class MainApp
+{
+    public static void Main()
+    {
+        int result = Calculator.plus(3, 4);
+        Console.WriteLine(result);
+
+        result = Calculator.Minus(5, 2);
+        Console.WriteLine(reresult);
+    }
+}
